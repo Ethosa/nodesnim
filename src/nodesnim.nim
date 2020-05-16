2f42d14f93168459c18544d027ff59da96be06bd
SakiKawasaki <theniklick@gmail.com>
2020-05-17 00:02:43 +0700

    add input.nim

--- src/nodesnim.nim
+++ src/nodesnim.nim
@@ -1,5 +1,6 @@
 import
   nodesnim/thirdparty/opengl,
+  nodesnim/thirdparty/opengl/glut,
 
   nodesnim/window,
   nodesnim/environment,
@@ -10,16 +11,17 @@ import
   nodesnim/core/anchor,
   nodesnim/core/color,
   nodesnim/core/exceptions,
+  nodesnim/core/input,
 
   nodesnim/nodes/node,
   nodesnim/nodes/scene,
-  nodesnim/nodes/canvas
+  nodesnim/nodes/canvas,
+
+  nodesnim/nodescontrol/control
 
 export
-  opengl,
-
+  opengl, glut,
   window, environment,
-
-  vector2, rect2, enums, anchor, color, exceptions,
-  
-  node, scene, canvas
+  vector2, rect2, enums, anchor, color, exceptions, input,
+  node, scene, canvas,
+  control
--- tests/test1.nim
+++ tests/test1.nim
@@ -2,13 +2,17 @@
 import nodesnim
 
 
-Window("hello world")
+Window(
+  "hello world",  # Window name
+  640,            # Window width,
+  360             # Window height
+)
 
 var
   mainobj: SceneObj
-  main = Scene("Main", mainobj)
+  main = Scene("Main", mainobj)  # Create pointer to Scene object.
 
 
-addScene(main)
-setMainScene("Main")
-windowLauch()
+addScene(main)        # Add new scene in window.
+setMainScene("Main")  # Set main scene.
+windowLauch()         # Start main loop.
