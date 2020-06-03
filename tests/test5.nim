c2e4faf6de1410b141bbfe4c0bfdaa92f90362e1
SakiKawasaki <social.ethosa@gmail.com>
2020-06-03 23:51:36 +0700

    fix test5.

--- tests/test5.nim
+++ tests/test5.nim
@@ -18,32 +18,32 @@ main.addChild(colorrect)
 colorrect.addChild(colorrect1)
 
 
-colorrect1.click =
-  proc(x, y: float) =  # This called when the user clicks on the Control node (ColorRect in this case).
+colorrect1.on_click =
+  proc(self: ControlPtr, x, y: float) =  # This called when the user clicks on the Control node (ColorRect in this case).
     colorrect1.move(3, 3)
 
-colorrect.press =
-  proc(x, y: float) =  # This called when the user holds on the mouse on the Control node.
+colorrect.on_press =
+  proc(self: ControlPtr, x, y: float) =  # This called when the user holds on the mouse on the Control node.
     colorrect.color.r -= 0.001
 
-colorrect.release =
-  proc(x, y: float) =  # This called when the user no more holds on the mouse.
+colorrect.on_release =
+  proc(self: ControlPtr, x, y: float) =  # This called when the user no more holds on the mouse.
     colorrect.color.r = 1
 
-colorrect.focus =
-  proc() =  # This called when the Control node gets focus.
+colorrect.on_focus =
+  proc(self: ControlPtr) =  # This called when the Control node gets focus.
     echo "hello ^^."
 
-colorrect.unfocus =
-  proc() =  # This called when the Control node loses focus.
+colorrect.on_unfocus =
+  proc(self: ControlPtr) =  # This called when the Control node loses focus.
     echo "bye :("
 
-colorrect1.mouse_enter =
-  proc(x, y: float) =  # This called when the mouse enters the Control node.
+colorrect1.on_mouse_enter =
+  proc(self: ControlPtr, x, y: float) =  # This called when the mouse enters the Control node.
     colorrect1.color = Color(1, 0.6, 1, 0.5)
 
-colorrect1.mouse_exit =
-  proc(x, y: float) =  # This called when the mouse exit from the Control node.
+colorrect1.on_mouse_exit =
+  proc(self: ControlPtr, x, y: float) =  # This called when the mouse exit from the Control node.
     colorrect1.color = Color(1f, 1f, 1f)
 
 
